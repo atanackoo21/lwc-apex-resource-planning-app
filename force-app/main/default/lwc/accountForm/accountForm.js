556eb6f5f6ad402400d36a307eee6a30c504c9b9
Your Name <you@example.com>
2021-10-05 20:09:17 +0200

    Add create new and delete function

--- force-app/main/default/lwc/accountForm/accountForm.js
+++ force-app/main/default/lwc/accountForm/accountForm.js
@@ -1,9 +1,13 @@
 import { api, LightningElement , track, wire} from 'lwc';
-import getAccounts from '@salesforce/apex/AccountHelper.getAccounts';
-import updateAccount from '@salesforce/apex/AccountHelper.updateAccount';
-
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import { NavigationMixin } from 'lightning/navigation';
 import { refreshApex } from '@salesforce/apex';
+
+import getAccounts from '@salesforce/apex/AccountHelper.getAccounts';
+import updateAccount from '@salesforce/apex/AccountHelper.updateAccount';
+import deleteAccount from '@salesforce/apex/AccountHelper.deleteAccount';
+import insertAccount from '@salesforce/apex/AccountHelper.insertAccount';
+
 
 const columns = [
     { label: 'Name', fieldName: 'Name',  type: 'text'},
@@ -39,6 +43,7 @@ const columns = [
 export default class AccountForm extends NavigationMixin(LightningElement) {
     @track isModalOpen;
     @track isModalDeleteOpen;
+    @track isNewModalOpen;
     @track clickedAccount={
         Name: "",
         Phone: "",
@@ -50,6 +55,7 @@ export default class AccountForm extends NavigationMixin(LightningElement) {
     closeModal(){
         this.isModalOpen = false;
         this.isModalDeleteOpen = false;
+        this.isNewModalOpen = false;
     }
     @track acc={Id: "", Name: ""};
     columns = columns;
@@ -78,11 +84,9 @@ export default class AccountForm extends NavigationMixin(LightningElement) {
             console.log('Delete button');
             this.isModalDeleteOpen = true;
         }
-        //console.log('Log rfrsh');
-        //return refreshApex(this.refreshTable);
     }
 
-    submitDetails(){
+    submitEdit(){
         updateAccount({ acc : this.clickedAccount})
         .then(result =>{
             console.log(result);
@@ -91,11 +95,72 @@ export default class AccountForm extends NavigationMixin(LightningElement) {
         .catch(error => {
             console.error(error);
         });
+        // this.clickedAccount = { ...this.clickedAccount, Name: event.target.value}
+        // this.clickedAccount = { ...this.clickedAccount, Phone: event.target.value }
+        // this.clickedAccount = { ...this.clickedAccount, Email__c: event.target.value }
+        // this.clickedAccount = { ...this.clickedAccount, BillingStreet: event.target.value } 
+        // this.clickedAccount = { ...this.clickedAccount, BillingCity: event.target.value } 
         this.isModalOpen = false;
     }
 
+    submitDelete(){
+        console.log('1234->' + this.clickedAccount.Id);
+        deleteAccount({ acc : this.clickedAccount})
+        .then(result =>{
+            console.log(result);
+            refreshApex(this.refreshTable);
+            this.dispatchEvent(
+                new ShowToastEvent({
+                    title: result == 'Successfully deleting' ? 'Success' : 'Error deleting record',
+                    message: result == 'Successfully deleting' ? 'Record deleted' : result,
+                    variant: result == 'Successfully deleting' ? 'success' : 'Error',
+                    mode: 'pester'
+                })
+            );
+        })
+        .catch(error => {
+            console.error(error);
+        });
+        // this.clickedAccount = { ...this.clickedAccount, Name: event.target.value}
+        // this.clickedAccount = { ...this.clickedAccount, Phone: event.target.value }
+        // this.clickedAccount = { ...this.clickedAccount, Email__c: event.target.value }
+        // this.clickedAccount = { ...this.clickedAccount, BillingStreet: event.target.value } 
+        // this.clickedAccount = { ...this.clickedAccount, BillingCity: event.target.value } 
+        this.isModalDeleteOpen = false;
+    }
+
+    submitCreate(){
+        const allValid = [...this.template.querySelectorAll('.addNew')]
+            .reduce((validSoFar, inputCmp) => {
+                        inputCmp.reportValidity();
+                        return validSoFar && inputCmp.checkValidity();
+            }, true);
+        if (allValid) {
+         insertAccount({ acc : this.clickedAccount})
+         .then(result =>{
+             console.log(result);
+             refreshApex(this.refreshTable);
+             this.dispatchEvent(
+                 new ShowToastEvent({
+                     title: result == 'Successfully adding' ? 'Success' : 'Error adding record',
+                     message: result == 'Successfully adding' ? 'Record added' : result,
+                     variant: result == 'Successfully adding' ? 'success' : 'Error',
+                     mode: 'pester'
+                 })
+             );
+         })
+         .catch(error => {
+             console.error(error);
+         });
+         
+         this.isNewModalOpens = false;
+        } else {
+            console.log('Invalid Input');
+        }
+    }
+
     handleChange(event){
-        console.log('13');
+        console.log(event.target.value);
 
         var targetName = event.target.name;
 
@@ -112,7 +177,8 @@ export default class AccountForm extends NavigationMixin(LightningElement) {
         }
     }
 
-    handleSelect(){
-        alert('Button clicked');
+    handleNew(){
+        //this.clickedAccount = null;
+        this.isNewModalOpen = true;
     }
 }
